3c8a0bd8ab284961c93d1b23aac2b61b7a8086b0
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-04-14 14:22:21 -0400

    Created an Add Task Method and finished basic logic

--- src/to/pkgdo/list/ToDoList.java
+++ src/to/pkgdo/list/ToDoList.java
@@ -14,20 +14,79 @@ public class ToDoList {
         
         boolean stay = true;
         
-        System.out.println("What would you like to do today? "
+        int option = 0;
+        
+        System.out.println("What would you like to do? "
                     + "\n1. Add a task! "
                     + "\n2. View Tasks "
                     + "\n3. Exit ");
         
+        int firstInput = 0;
+        
         while (stay){
             
-            //switch ()
+            if(firstInput > 0) {
+                 System.out.println("What would you like to do now? "
+                    + "\n1. Add a task! "
+                    + "\n2. View Tasks "
+                    + "\n3. Exit ");
+            }
             
+            boolean validOption = false;
+            
+            while(!validOption){
+                option = scanner.nextInt();
+                scanner.nextLine();
+                if (option > 0 && option < 4) {
+                    validOption = true;
+                }
+                else{
+                    System.out.println("BEEEEEP!!! Not an option, please choose 1-3.");
+                }
+            }
+            
+            switch(option){
+                case 1:
+                    System.out.println("What is the name of your task?: (Type done"
+                    + " if you dont want to add more tasks)");
+                    AddTask(scanner, tasks);
+                    break;
+                case 2:
+                System.out.println("-----------------------------------");
+                System.out.println("Here are your tasks: ");
+                // START > CHECK > STEP
+                for (int i = 0; i < tasks.size(); i++){
+                    System.out.println(i + 1 + ". " + tasks.get(i)); 
+                    // tasks.get, gets the String located in its index number
+                    // Which means, 0 is the first task and so on...
+                }
+                System.out.println("-----------------------------------");
+                break;
+                case 3:
+                System.out.println("Goodbyeee!");
+                stay = false;
+                break;
+            }
+            
+            firstInput++;
         }
         
         
         
         
+    }
+    
+    public static void AddTask(Scanner scanner, ArrayList<String> tasks){
+        boolean done = false;
+        while (!done){
+            String task = scanner.nextLine();
+            if (task.equalsIgnoreCase("done")){
+                done = true;
+            }
+            else{
+                tasks.add(task);
+            }
+        }
     }
     
 }
